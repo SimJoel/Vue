f4db71475bceb2d5c5de0c1f0dbf481ac128cf56
SimJoel <misosunh1@naver.com>
2021-05-28 11:24:32 +0900

    no message

---
+++ src/components/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+   <div class="p-5 text-center bg-light">
+    <h1 class="mb-3">Home</h1>
+    <h4 class="mb-3">Subheading</h4>
+    <a class="btn btn-primary" href="" role="button">Call to action</a>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
